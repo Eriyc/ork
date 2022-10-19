8cdee9bf5445f43e1cb3e82fb53412ef434ead48
Eric Dahlgren <eric@dgren.dev>
2022-10-19 14:28:46 +0200

    add start empty workout button

--- src/screens/templates.tsx
+++ src/screens/templates.tsx
@@ -1,8 +1,13 @@
 import React, {FC} from 'react';
 import {View, StyleSheet} from 'react-native';
+import {Button} from 'react-native-paper';
 
 const TemplatesScreen: FC = () => {
-  return <View style={[styles.container]}></View>;
+  return (
+    <View style={[styles.container]}>
+      <Button mode="contained-tonal">Start an empty workout</Button>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
