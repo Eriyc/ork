31883324fc5694dd7a4921468da501a7ea23deb2
Eric Dahlgren <eric@dgren.dev>
2022-10-19 13:59:31 +0200

    show last 3 workouts on overview page

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,21 +1,47 @@
-import {ThemeSwitcher} from '@/components';
-import {useMainNavigation} from '@/navigation';
+import {Link} from '@react-navigation/native';
 import React, {FC} from 'react';
 import {View, StyleSheet} from 'react-native';
-import {Card, Text} from 'react-native-paper';
+import { ScrollView } from 'react-native-gesture-handler';
+import {Surface, Text} from 'react-native-paper';
 
 const HomeScreen: FC = () => {
   return (
-    <Card>
-      <View style={[styles.container]}>
-        <Text>Gaming</Text>
+    <ScrollView style={[styles.container]}>
+      <View>
+        <View style={[styles.sectionHeader]}>
+          <Text variant="bodyLarge">History</Text>
+          <Link to="/history">
+            <Text variant="labelMedium">SHOW MORE</Text>
+          </Link>
+        </View>
+        <Surface style={[styles.historyCard]}>
+          <Text>Afternoon Workout</Text>
+          <Text variant="bodySmall">- 10 pushups</Text>
+        </Surface>
+        <Surface style={[styles.historyCard]}>
+          <Text>Morning Workout</Text>
+          <Text variant="bodySmall">- 10 situps</Text>
+        </Surface>
       </View>
-    </Card>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    padding: 8,
+  },
+  sectionHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  historyCard: {
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 12,
+  },
 });
 
 export {HomeScreen};
