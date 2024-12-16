ae33dccbdf491b37cb4da0d4544f2930bbc948f7
Eric <ericdahlgren03@gmail.com>
2024-12-16 15:15:52 +0100

    no need for a describe

--- src/features/authentication/__tests__/local-auth.test.ts
+++ src/features/authentication/__tests__/local-auth.test.ts
@@ -6,37 +6,35 @@ afterAll(() => {
   return authState$.reset();
 });
 
-describe("local auth", () => {
-  const mockExecute = jest.fn((command, params) => command.execute(params));
-  const auth = new Auth(new MockAuthExecutor(mockExecute));
+const mockExecute = jest.fn((command, params) => command.execute(params));
+const auth = new Auth(new MockAuthExecutor(mockExecute));
 
-  it("should start unauthenticated", () => {
-    expect(authState$.isAuthenticated.get()).toBe(false);
-  });
+it("should start unauthenticated", () => {
+  expect(authState$.isAuthenticated.get()).toBe(false);
+});
 
-  it("should be able to login", async () => {
-    await auth.login(new LocalLoginCommand(), undefined);
+it("should be able to login", async () => {
+  await auth.login(new LocalLoginCommand(), undefined);
 
-    const command = mockExecute.mock.calls[0][0];
-    expect(command).toBeInstanceOf(LocalLoginCommand);
+  const command = mockExecute.mock.calls[0][0];
+  expect(command).toBeInstanceOf(LocalLoginCommand);
 
-    expect(authState$.isAuthenticated.get()).toBe(true);
-  });
+  expect(authState$.isAuthenticated.get()).toBe(true);
+});
 
-  it("should be able to refresh", async () => {
-    const oldAccessToken = authState$.tokens.accessToken.peek();
-    expect(oldAccessToken).not.toBe(null);
+it("should be able to refresh", async () => {
+  const oldAccessToken = authState$.tokens.accessToken.peek();
+  expect(oldAccessToken).not.toBe(null);
 
-    await auth.refresh();
-    const newAccessToken = authState$.tokens.accessToken.peek();
+  await auth.refresh();
+  const newAccessToken = authState$.tokens.accessToken.peek();
 
-    expect(oldAccessToken).not.toBe(newAccessToken);
-    expect(authState$.isAuthenticated.get()).toBe(true);
-  });
+  expect(oldAccessToken).not.toBe(newAccessToken);
+  expect(authState$.isAuthenticated.get()).toBe(true);
+});
 
-  it("should be able to logout", async () => {
-    await auth.logout();
+it("should be able to logout", async () => {
+  await auth.logout();
 
-    expect(authState$.isAuthenticated.get()).toBe(false);
-  });
+  expect(authState$.isAuthenticated.get()).toBe(false);
 });
